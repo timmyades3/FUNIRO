0cb2d46714c56228ac20ae7d55f3d9f651ef9cfe
timmyades3 <oluwatimileyinadesina623@gmail.com>
2025-10-18 19:53:01 +0100

    added responsiveness

--- nav.js
+++ nav.js
@@ -1,16 +1,16 @@
-const sidebarItems = document.querySelectorAll('.sidebar li');
+const sidebarItems = document.querySelectorAll('.sidebar li, .ham');
 const bars = document.querySelector('#bars');
 const remove = document.querySelector('#remove');
 
-const mediaQuery = window.matchMedia('(max-width: 991px)');
+const mediaQuery = window.matchMedia('(max-width: 1024px)');
 
 function handleMediaQuery(mediaQuery) {
   if (mediaQuery.matches) {
-    
+
     sidebarItems.forEach(item => {
       item.addEventListener('click', () => {
         const sidebar = document.querySelector('.sidebar');
-        sidebar.style.right = '-1000px';
+        sidebar.style.right = '-200vw';
         remove.style.display = 'none';
         bars.style.display = 'block';
       });
@@ -18,19 +18,20 @@ function handleMediaQuery(mediaQuery) {
 
     bars.addEventListener('click', () => {
       const sidebar = document.querySelector('.sidebar');
-      sidebar.style.right = '0px';
+      sidebar.style.right = '2vw';
       remove.style.display = 'block';
       bars.style.display = 'none';
     });
 
     remove.addEventListener('click', () => {
       const sidebar = document.querySelector('.sidebar');
-      sidebar.style.right = '-1000px';
+      sidebar.style.right = '-200vw';
       remove.style.display = 'none';
       bars.style.display = 'block';
     });
+
   } else {
-   
+
     remove.style.display = 'none';
     bars.style.display = 'none';
   }
